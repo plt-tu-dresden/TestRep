53ebf98510f74f24803828c0935e24b508746940
zk <pg@ribuluo.local>
2016-05-30 23:38:28 +0800

    Initial Commit

---
+++ recovery/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  recovery
+//
+//  Created by zk on 30.05.16.
+//  Copyright © 2016 zk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
